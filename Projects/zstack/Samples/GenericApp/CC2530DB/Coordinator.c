90a576db03cff9dc99c9f0d90f3bac2c41828409
hsy1006 <1281068244@qq.com>
2021-05-11 15:17:12 +0800

    串口收发实验，修改Coordinator.c

--- Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.c
+++ Projects/zstack/Samples/GenericApp/CC2530DB/Coordinator.c
@@ -48,9 +48,11 @@ const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
 endPointDesc_t GenericApp_epDesc;//节点描述符
 byte GenericApp_TaskID;//任务优先级
 byte GenericApp_TransID;//数据发送序列号
+unsigned char uartbuf[128];
 /* 内部函数声明 ----------------------------------------------------------*/
 void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );//消息处理函数声明
 void GenericApp_SendTheMessage( void );//数据发送函数声明
+static void rxCB( uint8 port,uint8 event );//回调函数声明
 /* 函数 ------------------------------------------------------------------*/
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 * 函数名  ： GenericApp_Init
@@ -62,6 +64,7 @@ void GenericApp_SendTheMessage( void );//数据发送函数声明
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void GenericApp_Init( byte task_id )
 {
+    halUARTCfg_t uartConfig;
     GenericApp_TaskID             = task_id;//任务优先级初始化
     GenericApp_TransID            = 0;//数据发送序列号初始化
     //节点描述初始化
@@ -71,6 +74,11 @@ void GenericApp_Init( byte task_id )
         (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
     GenericApp_epDesc.latencyReq  = noLatencyReqs;
     afRegister( &GenericApp_epDesc );//将节点描述符进行注册
+    uartConfig.configured         = TRUE;//配置uart
+    uartConfig.baudRate           = HAL_UART_BR_115200;//波特率
+    uartConfig.flowControl        = FALSE;//流控制
+    uartConfig.callBackFunc       = rxCB;//回调函数
+    HalUARTOpen(0, &uartConfig);//串口初始化
 }
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 * 函数名  ： GenericApp_ProcessEvent
@@ -82,28 +90,45 @@ void GenericApp_Init( byte task_id )
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
 {
-    afIncomingMSGPacket_t *MSGpkt;//定义指向接收消息结构体的指针
-    if ( events & SYS_EVENT_MSG )
-    {
-        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive(GenericApp_TaskID );//从消息队列上接收消息
-        while ( MSGpkt )
-        {
-            switch ( MSGpkt->hdr.event )
-            {
-                case AF_INCOMING_MSG_CMD://对是否接收到消息进行判断
-                    GenericApp_MessageMSGCB( MSGpkt );
-                    break;
-                default:
-                    break;
-            }
-            osal_msg_deallocate( (uint8 *)MSGpkt );//处理接收到的消息后，释放存储空间
-            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive
-                ( GenericApp_TaskID );//再从消息队列里接收消息
-        }
-        return (events ^ SYS_EVENT_MSG);
-    }
-    return 0;
+//    afIncomingMSGPacket_t *MSGpkt;//定义指向接收消息结构体的指针
+//    if ( events & SYS_EVENT_MSG )
+//    {
+//        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive(GenericApp_TaskID );//从消息队列上接收消息
+//        while ( MSGpkt )
+//        {
+//            switch ( MSGpkt->hdr.event )
+//            {
+//                case AF_INCOMING_MSG_CMD://对是否接收到消息进行判断
+//                    GenericApp_MessageMSGCB( MSGpkt );
+//                    break;
+//                default:
+//                    break;
+//            }
+//            osal_msg_deallocate( (uint8 *)MSGpkt );//处理接收到的消息后，释放存储空间
+//            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive
+//                ( GenericApp_TaskID );//再从消息队列里接收消息
+//        }
+//        return (events ^ SYS_EVENT_MSG);
+//    }
+//    return 0;
 }
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： rxCB
+* 参数    ： uint8 port, uint8 event
+* 返回    ： void
+* 作者    ： 黄舒怡、李梦婷、郑一凡
+* 时间    ： 2021/5/11
+* 描述    ： 回调函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+static void rxCB(uint8 port, uint8 event)
+{
+    HalUARTRead(0, uartbuf,16);//从串口中读取数据并存放在uartbuf中
+    if(osal_memcmp(uartbuf,"www.wlwmaker.com",16))//判断接收到的字符
+    {
+        HalUARTWrite(0, uartbuf,16);//讲接收到的字符输出到串口
+    }
+}
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 * 函数名  ： GenericApp_MessageMSGCB
 * 参数    ： afIncomingMSGPacket_t *pkt 
