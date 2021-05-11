4f96dd47bb98925efdd96755efdb1aec6b25cff2
hsy1006 <1281068244@qq.com>
2021-05-11 09:35:56 +0800

    添加终端节点驱动文件，烧录完毕

---
+++ Projects/zstack/Samples/GenericApp/CC2530DB/Enddevice.c
@@ -0,0 +1,137 @@
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 文件名  ： Enddevice
+* 作者    ： 黄舒怡、李梦婷、郑一凡
+* 版本    ： V1.0.0
+* 时间    ： 2021/5/10
+* 简要    ： 终端节点驱动文件
+********************************************************************
+* 副本
+*
+*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+/* 头文件 ----------------------------------------------------------------*/
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDApp.h"
+#include "ZDObject.h"
+#include "ZDProfile.h"
+#include <string.h>
+#include "Coordinator.h"
+#include "DebugTrace.h"
+#if !defined( WIN32 )
+#include "OnBoard.h"
+#endif
+#include "hal_lcd.h"
+#include "hal_led.h"
+#include "hal_key.h"
+#include "hal_uart.h"
+/* 宏定义 ----------------------------------------------------------------*/
+/* 结构体或枚举 ----------------------------------------------------------*/
+//演示如何实现点对点通信
+const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] = 
+{
+    GENERICAPP_CLUSTERID
+};
+//简单设备描述
+const SimpleDescriptionFormat_t GenericApp_SimpleDesc = 
+{
+    GENERICAPP_ENDPOINT,
+    GENERICAPP_PROFID,
+    GENERICAPP_DEVICEID,
+    GENERICAPP_DEVICE_VERSION,
+    GENERICAPP_FLAGS,
+    0,
+    (cId_t *)NULL,
+    GENERICAPP_MAX_CLUSTERS,
+    (cId_t *)GenericApp_ClusterList
+};
+//定义四个变量
+endPointDesc_t GenericApp_epDesc;//节点描述符
+byte GenericApp_TaskID;//任务优先级
+byte GenericApp_TransID;//数据发送序列号
+devStates_t GenericApp_NwkState;//保存节点状态
+/* 内部函数声明 ----------------------------------------------------------*/
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );//消息处理函数声明
+void GenericApp_SendTheMessage( void );//数据发送函数声明
+/* 函数 ------------------------------------------------------------------*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： GenericApp_Init
+* 参数    ： byte task_id
+* 返回    ： void
+* 作者    ： 黄舒怡、李梦婷、郑一凡
+* 时间    ： 2021/5/10
+* 描述    ： 任务初始化函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void GenericApp_Init( byte task_id )
+{
+    GenericApp_TaskID             = task_id;//优先级初始化
+    GenericApp_NwkState           = DEV_INIT;//初始化设备状态
+    GenericApp_TransID            = 0;//数据发送序列号初始化
+    //节点描述初始化
+    GenericApp_epDesc.endPoint    = GENERICAPP_ENDPOINT;
+    GenericApp_epDesc.task_id     = &GenericApp_TaskID;
+    GenericApp_epDesc.simpleDesc  =
+        (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
+    GenericApp_epDesc.latencyReq  = noLatencyReqs;
+    afRegister( &GenericApp_epDesc );//将节点描述符进行注册
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： GenericApp_ProcessEvent
+* 参数    ： byte task_id, Uint16 event
+* 返回    ： UINT16
+* 作者    ： 黄舒怡、李梦婷、郑一凡
+* 时间    ： 2021/5/10
+* 描述    ： 消息处理函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
+{
+    afIncomingMSGPacket_t *MSGpkt;//定义指向接收消息结构体的指针
+    if ( events & SYS_EVENT_MSG )
+    {
+        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive(GenericApp_TaskID );//从消息队列上接收消息
+        while ( MSGpkt )
+        {
+            switch ( MSGpkt->hdr.event )
+            {
+                case ZDO_STATE_CHANGE:
+                    GenericApp_NwkState = (devStates_t)(MSGpkt->hdr.status);//读取节点设备类型
+                    if (GenericApp_NwkState == DEV_END_DEVICE)//实现数据发送
+                    {
+                        GenericApp_SendTheMessage() ;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            osal_msg_deallocate( (uint8 *)MSGpkt );//处理接收到的消息后，释放存储空间
+            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive
+                ( GenericApp_TaskID );//再从消息队列里接收消息
+        }
+        return (events ^ SYS_EVENT_MSG);
+    }
+    return 0;
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： GenericApp_SendTheMessage
+* 参数    ： void
+* 返回    ： void
+* 作者    ： 黄舒怡、李梦婷、郑一凡
+* 时间    ： 2021/5/10
+* 描述    ： 消息发送函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void GenericApp_SendTheMessage( void )
+{
+    unsigned char theMessageData[4] = "LED";//存放要发送的数据
+    afAddrType_t my_DstAddr;
+    my_DstAddr.addrMode = (afAddrMode_t)Addr16Bit;
+    my_DstAddr.endPoint = GENERICAPP_ENDPOINT;
+    my_DstAddr.addr.shortAddr = 0x0000;
+    AF_DataRequest( &my_DstAddr, &GenericApp_epDesc,GENERICAPP_CLUSTERID, 3,
+                       theMessageData,
+                       &GenericApp_TransID,
+                       AF_DISCV_ROUTE,
+                       AF_DEFAULT_RADIUS );//无线数据的发送
+    HalLedBlink(HAL_LED_2,0,50,500);
+}
+                                 
+    
+                    
